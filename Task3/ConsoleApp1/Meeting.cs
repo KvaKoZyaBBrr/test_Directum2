f7b41c2bbc1af31afcc44e002ebdaf444999df61
amuratov <amuratov@ngpinform.ru>
2022-08-21 03:21:42 +0500

    Task3. Menu

---
+++ Task3/ConsoleApp1/Meeting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Meeting
+    {
+        string Name;
+        DateTime Start;
+        DateTime End;
+        DateTime Notify;
+
+        public Meeting(string name,  DateTime start, DateTime end)
+        {
+            Name = name;
+            Start = start;
+            End = end;
+        }
+
+        public Meeting(string name, DateTime start, DateTime end, DateTime notify) {
+            Name = name;
+            Start = start;
+            End = end;
+            Notify = notify;
+        }
+
+        public bool SetNotify(DateTime time) {
+            Notify = time;
+            return true;
+        }
+    }
+}
---
+++ Task3/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        List<Meeting> meetingList = new List<Meeting>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose operation");
+                Console.WriteLine("1. Add new meeting");
+                Console.WriteLine("2. Change  meeting");
+                Console.WriteLine("3. View meetings");
+                Console.WriteLine("4. Export meetings");
+                Console.WriteLine("0. Exit");
+
+                int command;
+                string rawCommand = Console.ReadLine();
+                bool parserResult = Int32.TryParse(rawCommand, out command);
+                if (parserResult) {
+                    switch (command) {
+                        case (1): { AddNewMeeting(); break; }
+                        case (2): { ChangeMeeting(); break; }
+                        case (3): { ViewMeeting(); break; }
+                        case (4): { ExportMeeting(); break; }
+                        case (0):return;
+                    }
+                }
+            }
+        }
+
+        static void AddNewMeeting() { 
+            
+        }
+        static void ChangeMeeting()
+        {
+
+        }
+        static void ViewMeeting()
+        {
+
+        }
+        static void ExportMeeting()
+        {
+
+        }
+    }
+}
